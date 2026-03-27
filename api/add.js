aee08c153ce0a6298bd690d7bfdefb7f8330e0e1
Oshney Thakur <152894305+Oshney@users.noreply.github.com>
2026-03-27 10:26:14 +0530

    Update add.js

--- api/add.js
+++ api/add.js
@@ -1,25 +1,35 @@
 export default async function handler(req, res) {
-
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Only POST allowed" });
   }
 
-  const { item, qty } = req.body;
+  try {
+    const { item, qty } = req.body;
 
-  const response = await fetch(
-    "https://locrzxuubbbiwtoefyht.supabase.co/rest/v1/stock",
-    {
-      method: "POST",
-      headers: {
-        apikey: process.env.KEY,
-        Authorization: `Bearer ${process.env.KEY}`,
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify([{ item, qty }])
-    }
-  );
+    const response = await fetch(
+      "https://locrzxuubbbiwtoefyht.supabase.co/rest/v1/stock",
+      {
+        method: "POST",
+        headers: {
+          apikey: process.env.KEY,
+          Authorization: `Bearer ${process.env.KEY}`,
+          "Content-Type": "application/json",
+          Prefer: "return=representation"
+        },
+        body: JSON.stringify([{ item, qty }]),
+      }
+    );
 
-  const data = await response.json();
+    const data = await response.json();
 
-  res.status(200).json(data);
+    res.status(200).json({
+      message: "Added successfully",
+      data: data
+    });
+
+  } catch (err) {
+    res.status(500).json({
+      error: err.message,
+    });
+  }
 }
